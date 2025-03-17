cc7f8c7151bb8b383df1f28c711a6ab6758c1539
EisaHussain <156790060+EisaHussain@users.noreply.github.com>
2025-03-17 23:05:27 +0000

    Update Program.cs

--- StartEndCodeFixingTask/Program.cs
+++ StartEndCodeFixingTask/Program.cs
@@ -4,12 +4,34 @@
     {
         static void Main(string[] args)
         {
-            // The algorithm in the Readme contains an error
-            // Rewrite the algorithm in C# bellow fixing the error
-            // Show the test outputs for the test table in the Readme
-            // Put your evidence where it says Do the coding task in C#
+            int StartYear = 0;
+            int EndYear = 0;
+            int difference = 0;
+            bool validChoice = false;
+            do
+            {
+                Console.WriteLine("Enter start year");
+                StartYear = Convert.ToInt32(Console.ReadLine());   
+                Console.WriteLine("Enter end year");
+                EndYear = Convert.ToInt32(Console.ReadLine());
+                if (StartYear > EndYear)
+                {
+                    Console.WriteLine("Start year must be before end year");
+                }
+                else
+                {
+                    if (StartYear > 2000)
+                    {
+                        Console.WriteLine("Start year must be before 200");
+                    }
+                    else
+                    {
+                        validChoice = true;
+                    }
+                }
 
-
+            } while (validChoice);
+            difference = EndYear - StartYear;
         }
     }
 }
